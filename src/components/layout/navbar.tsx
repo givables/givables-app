4937451b7ae23803c43088cf2225431504934fff
axantillon <aantillon.leiva@gmail.com>
2021-10-19 15:38:17 -0600

    fix navbar for mobile

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,7 +3,7 @@ import Wallet from "../web3/wallet";
 export default function Navbar() {
 
     return (
-        <div className="flex items-center justify-between w-full h-20 px-12">
+        <div className="flex items-center justify-between w-full h-20 px-4 sm:px-12">
             <div className="flex items-center">
                 <img className="object-contain w-full h-16" src="/assets/logo/logo.png" alt="" />
             </div>
