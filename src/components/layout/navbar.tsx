15abea50ed460382b300869255cc63b63badeec2
axantillon <aantillon.leiva@gmail.com>
2021-10-19 15:41:54 -0600

    shrink logo on mobile :)

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <div className="flex items-center justify-between w-full h-20 px-4 sm:px-12">
             <div className="flex items-center">
-                <img className="object-contain w-full h-16" src="/assets/logo/logo.png" alt="" />
+                <img className="object-contain w-full h-12 sm:h-16" src="/assets/logo/logo.png" alt="" />
             </div>
 
             <div className="flex items-center">
