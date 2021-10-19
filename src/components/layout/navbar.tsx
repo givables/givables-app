9baafa61f56cbd124f6c2ec9a2e49fc3a2c41127
axantillon <aantillon.leiva@gmail.com>
2021-10-19 13:56:39 -0600

    init commit

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+
+    return (
+        <div className="flex items-center justify-between w-full h-24">
+            <div className="">
+
+            </div>
+            <div className="">
+                
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Footer from '../components/layout/footer'
+import Navbar from '../components/layout/navbar'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen py-2">
+      <Head>
+        <title>Givables</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <nav>
+        <Navbar/>
+      </nav>
+
+      <main className="flex flex-col flex-grow items-center justify-center w-full">
+
+      </main>
+
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
+  )
+}
