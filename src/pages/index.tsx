59534135ae16888437591cf7dfe2b4766b22eec8
axantillon <aantillon.leiva@gmail.com>
2021-11-10 10:34:08 -0600

    add static image background to mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import logo from '../../public/assets/logo/logo.png'
+import background from '../../public/assets/background.png'
 import WaitlistButton from '../components/tools/waitlistButton'
 
 export default function Home() {
 
   return (
-    <div className="relative flex flex-col min-h-screen">
+    <div className="relative flex flex-col w-screen min-h-screen" style={{
+      backgroundImage:  `url(${"/assets/background_img.png"})`,
+    }}>
       <Head>
         <title>Givables</title>
       </Head>
 
-      <video autoPlay muted loop className="z-0 absolute top-0 left-0 w-screen h-full" style={{objectFit: 'cover'}}>
+      <video autoPlay muted loop className="hidden sm:block absolute z-0 top-0 left-0 w-screen h-full" style={{objectFit: 'cover'}}>
         <source src='./assets/background.mp4' type="video/mp4"/>
       </video>
       <main className="z-10 relative flex flex-col flex-grow items-center justify-center w-full">
