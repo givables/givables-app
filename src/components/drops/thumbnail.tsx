265262170bdd485020549f6e2398bac94d1f637f
axantillon <aantillon.leiva@gmail.com>
2021-11-21 18:29:39 -0600

    add more art

--- src/components/drops/thumbnail.tsx
+++ src/components/drops/thumbnail.tsx
@@ -5,7 +5,7 @@ export default function Thumbnail({piece: piece}) {
     return (
         <Link href="/drops/1/1">
             <div className="relative w-64 h-64 m-2 rounded-lg" style={{ backgroundImage: `url(${piece.image})`, backgroundRepeat:"no-repeat", backgroundSize: "110% 110%", backgroundPositionX: '50%', backgroundPositionY:'35%'}}>
-                <div className="z-10 absolute top-0 left-0 flex justify-center items-center w-full h-full opacity-0 hover:opacity-100 bg-black bg-opacity-25 cursor-pointer transition duration-200 ease-in-out">
+                <div className="z-10 absolute top-0 left-0 flex justify-center items-center w-full h-full rounded-lg opacity-0 hover:opacity-100 bg-black bg-opacity-25 cursor-pointer transition duration-200 ease-in-out">
                     <div className="py-1 px-2 bg-black bg-opacity-25">
                         <span className="uppercase leading-none text-white text-sm font-semibold"> {piece.artist.name} </span>
                     </div>
--- src/pages/api/drops/[dropId].ts
+++ src/pages/api/drops/[dropId].ts
@@ -107,6 +107,91 @@ export default function handler(req, res) {
           website: "https://www.owenmccallumkeeler.com/",
         },
       },
+      {
+        id: "0",
+        name: "Stretching at Dusk",
+        image: "/assets/art/6.png",
+        description: {
+          text: "Stretching at Dusk is a still image from the animation Stretching at Dusk",
+          date: "Circa January 2021",
+          dimensions: "4000px x 3750px",
+        },
+        artist: {
+          name: "Owen McCallum-Keeler",
+          about:
+            "OWEN MCCALLUM-KEELER IS A IS A RISD PAINTING MAJOR (C0 2023).THE GIVABLES RISD COLLECTION FEATURES WORK FROM CURRENT AND RECENTLY GRADUATED, UP AND COMING DESIGNERS OF VAR OUS MAJORS FROM THE RHODE IsLAND SCHO00L OF DESIGN (RISD). RISD ISA WORLD CLASS INSTITUTION, AND THE PREMIERE ART AND DESIGN SCHOOL OF THE UNITED STATES.",
+          instagram: "https://www.instagram.com/owenmcckee/",
+          website: "https://www.owenmccallumkeeler.com/",
+        },
+      },
+      {
+        id: "0",
+        name: "Stretching at Dusk",
+        image: "/assets/art/7.png",
+        description: {
+          text: "Stretching at Dusk is a still image from the animation Stretching at Dusk",
+          date: "Circa January 2021",
+          dimensions: "4000px x 3750px",
+        },
+        artist: {
+          name: "Owen McCallum-Keeler",
+          about:
+            "OWEN MCCALLUM-KEELER IS A IS A RISD PAINTING MAJOR (C0 2023).THE GIVABLES RISD COLLECTION FEATURES WORK FROM CURRENT AND RECENTLY GRADUATED, UP AND COMING DESIGNERS OF VAR OUS MAJORS FROM THE RHODE IsLAND SCHO00L OF DESIGN (RISD). RISD ISA WORLD CLASS INSTITUTION, AND THE PREMIERE ART AND DESIGN SCHOOL OF THE UNITED STATES.",
+          instagram: "https://www.instagram.com/owenmcckee/",
+          website: "https://www.owenmccallumkeeler.com/",
+        },
+      },
+      {
+        id: "0",
+        name: "Stretching at Dusk",
+        image: "/assets/art/8.png",
+        description: {
+          text: "Stretching at Dusk is a still image from the animation Stretching at Dusk",
+          date: "Circa January 2021",
+          dimensions: "4000px x 3750px",
+        },
+        artist: {
+          name: "Owen McCallum-Keeler",
+          about:
+            "OWEN MCCALLUM-KEELER IS A IS A RISD PAINTING MAJOR (C0 2023).THE GIVABLES RISD COLLECTION FEATURES WORK FROM CURRENT AND RECENTLY GRADUATED, UP AND COMING DESIGNERS OF VAR OUS MAJORS FROM THE RHODE IsLAND SCHO00L OF DESIGN (RISD). RISD ISA WORLD CLASS INSTITUTION, AND THE PREMIERE ART AND DESIGN SCHOOL OF THE UNITED STATES.",
+          instagram: "https://www.instagram.com/owenmcckee/",
+          website: "https://www.owenmccallumkeeler.com/",
+        },
+      },
+      {
+        id: "0",
+        name: "Stretching at Dusk",
+        image: "/assets/art/9.jpg",
+        description: {
+          text: "Stretching at Dusk is a still image from the animation Stretching at Dusk",
+          date: "Circa January 2021",
+          dimensions: "4000px x 3750px",
+        },
+        artist: {
+          name: "Owen McCallum-Keeler",
+          about:
+            "OWEN MCCALLUM-KEELER IS A IS A RISD PAINTING MAJOR (C0 2023).THE GIVABLES RISD COLLECTION FEATURES WORK FROM CURRENT AND RECENTLY GRADUATED, UP AND COMING DESIGNERS OF VAR OUS MAJORS FROM THE RHODE IsLAND SCHO00L OF DESIGN (RISD). RISD ISA WORLD CLASS INSTITUTION, AND THE PREMIERE ART AND DESIGN SCHOOL OF THE UNITED STATES.",
+          instagram: "https://www.instagram.com/owenmcckee/",
+          website: "https://www.owenmccallumkeeler.com/",
+        },
+      },
+      {
+        id: "0",
+        name: "Stretching at Dusk",
+        image: "/assets/art/10.png",
+        description: {
+          text: "Stretching at Dusk is a still image from the animation Stretching at Dusk",
+          date: "Circa January 2021",
+          dimensions: "4000px x 3750px",
+        },
+        artist: {
+          name: "Owen McCallum-Keeler",
+          about:
+            "OWEN MCCALLUM-KEELER IS A IS A RISD PAINTING MAJOR (C0 2023).THE GIVABLES RISD COLLECTION FEATURES WORK FROM CURRENT AND RECENTLY GRADUATED, UP AND COMING DESIGNERS OF VAR OUS MAJORS FROM THE RHODE IsLAND SCHO00L OF DESIGN (RISD). RISD ISA WORLD CLASS INSTITUTION, AND THE PREMIERE ART AND DESIGN SCHOOL OF THE UNITED STATES.",
+          instagram: "https://www.instagram.com/owenmcckee/",
+          website: "https://www.owenmccallumkeeler.com/",
+        },
+      },
     ],
   });
 }
